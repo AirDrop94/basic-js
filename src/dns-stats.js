742762e76f33da69ad866bfd4a98e560a72083b5
Artyom <art161994@yandex.ru>
2024-12-28 19:00:36 +0100

    feat: add solutions for the top five tasks

--- src/dns-stats.js
+++ src/dns-stats.js
@@ -22,9 +22,19 @@ const { NotImplementedError } = require('../extensions/index.js');
  * }
  *
  */
-function getDNSStats(/* domains */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getDNSStats(domains) {
+ const result = {};
+
+  domains.forEach(domain => {
+    const parts = domain.split('.').reverse();
+    let key = '';
+    parts.forEach(part => {
+      key = `${key}.${part}`;
+      result[key] = (result[key] || 0) + 1;
+    });
+  });
+
+  return result;
 }
 
 module.exports = {
